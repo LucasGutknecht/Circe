23c5a2db7b17433bee488861c2abc1a3002e5fb4
Lucas <lucas@albesbrasil.com.br>
2025-08-30 12:03:21 -0300

    first commit

---
+++ circe.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main(int argc, char** argv) {
+    puts("Circe Version 0.0.0.0.1");
+    puts("Press Ctrl+c to Exit\n");
+
+    while (1) {
+        char* input = readline("Circe> ");
+        add_history(input);
+        printf("No you're a %s\n", input);
+        free(input);
+    }
+
+    return 0;
+}

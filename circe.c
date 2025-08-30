3ae3809eab2f3f7e1be08b917a04a502ec326576
Lucas <lucas@albesbrasil.com.br>
2025-08-30 12:06:10 -0300

    Adicionado lista dos proximos passos.

--- circe.c
+++ circe.c
@@ -3,6 +3,10 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+// Proximos passos
+// 1. Adicionar os itens de preprocessador.
+// 2. Adicionar os Parser Combinators
+
 int main(int argc, char** argv) {
     puts("Circe Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");

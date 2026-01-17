6fdd022a849872c921517c22e0fc69c2f6556af1
Lucas <lucas@albesbrasil.com.br>
2026-01-17 12:27:18 -0300

    Adicionado tratamento de erros.

--- parsing.c
+++ parsing.c
@@ -27,32 +27,88 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
-/* Usando o operador String para ver qual operacao deve-se realizar */
-long eval_op(long x, char* op, long y) {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    return 0;
+/* Definindo códigos para os erros possíveis. */
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+/* Definindo tipos de valores possíveis. */
+enum { LVAL_NUM, LVAL_ERR };
+
+/* Definindo o novo tipo lval */
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+/* Função para criar um novo lval numérico */
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
 }
 
-long eval(mpc_ast_t* t) {
+/* Função para criar um novo lval de erro */
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+/* Printar um lval */
+void lval_print(lval v) {
+    switch (v.type) {
+        case LVAL_NUM: printf("%li", v.num); break;
+        case LVAL_ERR:
+            if (v.err == LERR_DIV_ZERO) { printf("Erro: Divisao por 0!"); }
+            if (v.err == LERR_BAD_OP)   { printf("Erro: Operador invalido!"); }
+            if (v.err == LERR_BAD_NUM)  { printf("Error: Número invalido!"); }
+        break;
+    }
+}
+
+/* Printar um lval com nova linha */
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
+/* Usando o operador String para ver qual operacao deve-se realizar */
+lval eval_op(lval x, char* op, lval y) {
+    
+    /* Se o valor either é um erro, retorna ele */
+    if(x.type == LVAL_ERR) { return x; }
+    if(y.type == LVAL_ERR) { return y; }
+
+    /* De outra maneira, realiza a operacao */
+    if(strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+    if(strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+    if(strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+    if(strcmp(op, "/") == 0) { 
+        /* Checa por divisao por 0 */
+        return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
+    }
+
+    return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t* t) {
     /* Se o nó é um número */
     if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+        /* Verifica se existe erro na conversão */
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
     }
 
     /* O operador é o segundo filho */
     char* op = t->children[1]->contents;
 
     /* Avaliando o primeiro operando */
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     /* Iterando sobre os outros operandos e aplicando o operador */
     int i = 3;
     while (strstr(t->children[i]->tag, "expr")) {
-        long y = eval(t->children[i]);
-        x = eval_op(x, op, y);
+        x = eval_op(x, op, eval(t->children[i]));
         i++;
     }
 
@@ -87,8 +143,8 @@ int main(int argc, char** argv) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Circe, &r)) {
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);

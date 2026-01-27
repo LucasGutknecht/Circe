330cb75e98e75d6a9eb92a87c40a8fe6d1de4766
Lucas <lucas@albesbrasil.com.br>
2026-01-27 12:47:30 -0300

    Concluído a adicao de Sexpr.

--- parsing.c
+++ parsing.c
@@ -27,10 +27,6 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
-
-/* Definindo códigos para os erros possíveis. */
-enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
-
 /* Definindo tipos de valores possíveis. */
 enum {LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR};
 
@@ -114,14 +110,12 @@ lval* lval_add(lval* v, lval* x) {
 }
 
 /* Printar um lval */
-void lval_print(lval v) {
-    switch (v.type) {
-        case LVAL_NUM: printf("%li", v.num); break;
-        case LVAL_ERR:
-            if (v.err == LERR_DIV_ZERO) { printf("Erro: Divisao por 0!"); }
-            if (v.err == LERR_BAD_OP)   { printf("Erro: Operador invalido!"); }
-            if (v.err == LERR_BAD_NUM)  { printf("Error: Número invalido!"); }
-        break;
+void lval_print(lval* v) {
+    switch (v->type) {
+        case LVAL_NUM: printf("%li", v->num); break;
+        case LVAL_ERR: printf("Error: %s", v.err); break;
+        case LVAL_SYM: printf("%s", v->sym); break;
+        case LVAL_SEXPR: lval_expr_print(v, '(', ')');
     }
 }
 
@@ -129,7 +123,7 @@ void lval_print(lval v) {
 void lval_println(lval v) { lval_print(v); putchar('\n'); }
 
 /* Usando o operador String para ver qual operacao deve-se realizar */
-lval eval_op(lval* v, int i) {
+lval* lval_pop(lval* v, int i) {
     
     /* Pegando o item em i */
     lval* x = v->cell[i];
@@ -146,36 +140,141 @@ lval eval_op(lval* v, int i) {
     return x;
 }
 
-lval eval(mpc_ast_t* t) {
-    /* Se o nó é um número */
-    if (strstr(t->tag, "number")) {
-        /* Verifica se existe erro na conversão */
-        errno = 0;
-        long x = strtol(t->contents, NULL, 10);
-        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
-    }
-
-    /* O operador é o segundo filho */
-    char* op = t->children[1]->contents;
-
-    /* Avaliando o primeiro operando */
-    lval x = eval(t->children[2]);
-
-    /* Iterando sobre os outros operandos e aplicando o operador */
-    int i = 3;
-    while (strstr(t->children[i]->tag, "expr")) {
-        x = eval_op(x, op, eval(t->children[i]));
-        i++;
-    }
-
+lval* lval_take(lval* v, int i) {
+    lval* x = lval_pop(v, i);
+    lval_del(v);
     return x;
+}
+
+void lval_expr_print(lval v, char open, char close) {
+    putchar(open);
+    for (int i = 0; i < v.count; i++) {
+        lval_print(*v.cell[i]);
+        if (i != (v.count - 1)) {
+            putchar(' ');
+        }
+    }
+    putchar(close);
+}
+
+lval* builtin_op(lval* a, char* op) {
+    for(int i = 0; i < a->count; i++) {
+        /* Verifica se todos os elementos são números */
+        if (a->cell[i]->type != LVAL_NUM) {
+            lval_del(a);
+            return lval_err("Operador não pode operar sobre tipos não números!");
+        }
+    }
+    lval* x = lval_pop(a, 0);
+    /* Se o operador é '-', e só tem um operando, faz a negação */
+    if ((strcmp(op, "-") == 0) && a->count == 0) {
+        x->num = -x->num;           
+    }
+
+    while (a->count > 0) {
+        lval* y = lval_pop(a, 0);
+
+        if (strcmp(op, "+") == 0) { x->num += y->num; }
+        if (strcmp(op, "-") == 0) { x->num -= y->num; }
+        if (strcmp(op, "*") == 0) { x->num *= y->num; }
+        if (strcmp(op, "/") == 0) {
+            if (y->num == 0) {
+                lval_del(x);
+                lval_del(y);
+                x = lval_err("Erro: Divisão por zero!");
+                break;
+            }
+            x->num /= y->num;
+        }
+
+        lval_del(y);
+    }
+
+    lval_del(a);
+    return x;
+}   
+
+lval* lval_eval(lval* v);
+
+lval* lval_eval_sexpr(lval* v) {
+    /* Avaliando todas as células */
+    for (int i = 0; i < v->count; i++) {
+        v->cell[i] = lval_eval(v->cell[i]);
+    }
+
+    /* Verificando por erros */
+    for (int i = 0; i < v->count; i++) {
+        if (v->cell[i]->type == LVAL_ERR) {
+            return lval_take(v, i);
+        }
+    }
+
+    /* Se a expressão está vazia, retornar ela */
+    if (v->count == 0) {
+        return v;
+    }
+
+    /* Se a expressão tem apenas um elemento, retornar esse elemento */
+    if (v->count == 1) {
+        return lval_take(v, 0);
+    }
+
+    /* Garantir que o primeiro elemento é um símbolo */
+    lval* f = lval_pop(v, 0);
+    if (f->type != LVAL_SYM) {
+        lval_del(f);
+        lval_del(v);
+        return lval_err("Primeiro elemento não é um operador!");
+    }
+
+    /* Aplicar o operador */
+    lval* result = builtin_op(v, f->sym);
+    lval_del(f);
+    return result;
+}
+
+lval* lval_eval(lval* v) {
+    /* Se o lval é uma expressão S */
+    if (v->type == LVAL_SEXPR) {
+        return lval_eval_sexpr(v);
+    }
+    /* Senão, retornar o próprio lval */
+    return v;
+}
+
+lval* lval_read_num(mpc_ast_t* t) {
+    errno = 0;
+    long x = strtol(t->contents, 10, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err("Número inválido!");
+}
+
+lval* lval_read(mpc_ast_t* t){
+    /* Se o simbolo ou número retorna a conversao daquele tipo */
+    if (strstr(t->tag, "number")) { return lval_read_num(t); }
+    if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+    /* Se for root (>) ou sexpr */
+    lval* x = NULL;
+    if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+    if (strstr(t->tag, "sexpr")) { x = lval_sexpr(); }
+
+    /* Preenchendo essas lista com qualquer expressao valida */
+    for (int i = 0; i < t->children_num; i++) {
+        if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
+        if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
+        x = lval_add(x, lval_read(t->children[i]));     
+    }
 }
 
 int main(int argc, char** argv) {
 
     /* Criando parsers */
     mpc_parser_t* Number = mpc_new("number");
-    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Operator = mpc_new("symbol");
+    mpc_parser_t* Sexpr = mpc_new("sexpr");
     mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* Circe = mpc_new("circe");
 
@@ -185,12 +284,12 @@ int main(int argc, char** argv) {
         "                                                     \
             number   : /-?[0-9]+/ ;                           \
             operator : '+' | '-' | '*' | '/' ;                \
-            expr     : <number> | '(' <operator> <expr>+ ')' ;\
-            circe    : /^/ <operator> <expr>+ /$/ ;           \
+            expr     : <number> | <symbol> | <sexpr> ;\
+            circe    : /^/ <expr>* /$/ ;           \
         ",
-        Number, Operator, Expr, Circe);
+        Number, Symbol, Sexpr, Expr, Circe);
 
-    puts("Circe Version 0.0.0.0.1");
+    puts("Circe Version 0.0.0.0.5");
     puts("Press Ctrl+c to Exit\n");
 
     while (1) {
@@ -199,20 +298,21 @@ int main(int argc, char** argv) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Circe, &r)) {
-            lval result = eval(r.output);
-            lval_println(result);
+            lval* x = lval_eval(lval_read(r.output));
+            lval_println(x);
+            lval_del(x);
             mpc_ast_delete(r.output);
-        } else {
+        }else {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
 
-        /* Liberando e deletando parsers */
-        mpc_cleanup(4, Number, Operator, Expr, Circe);
-
         /* Liberando a memória alocada para a entrada */
         free(input);
     }
+    
+    /* Liberando e deletando parsers */
+    mpc_cleanup(4, Number, Operator, Expr, Circe);
 
     return 0;
 }
